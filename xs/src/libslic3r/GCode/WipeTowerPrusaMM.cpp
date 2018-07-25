13be1f1abf6295a5b6c042782acd7d4ede2d6fc0
mcm001 <matthew.morley.ca@gmail.com>
2018-07-25 12:01:20 -0700

    Update WipeTowerPrusaMM.cpp

--- xs/src/libslic3r/GCode/WipeTowerPrusaMM.cpp
+++ xs/src/libslic3r/GCode/WipeTowerPrusaMM.cpp
@@ -852,7 +852,7 @@ void WipeTowerPrusaMM::toolchange_Change(
 	case FLEX:  speed_override = 35; break;
 	default:    speed_override = 100;
 	}
-	writer.append("CHANGE_TOOL ");	
+	writer.append("A602 ");	
 	writer.set_tool(new_tool)
 	      .speed_override(speed_override)
 	      .flush_planner_queue();
